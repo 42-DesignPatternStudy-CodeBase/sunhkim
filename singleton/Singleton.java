aeb87a448879d0b8d00ee5226bf92670d4598e4d
Sunhye Kim <sunhkim@cx2r5s2.42seoul.kr>
2023-02-06 19:55:30 +0900

    feat: bill pugh singleton

---
+++ singleton/Singleton.java
@@ -0,0 +1,13 @@
+package singleton;
+
+public class Singleton {
+    private singleton() {}
+
+    private static class SingletonHelper {
+        private static final Singleton INSTANCE = new Singleton();
+    }
+
+    public static Singleton getInstance() {
+        return SingletonHelper.INSTANCE;
+    }
+}
